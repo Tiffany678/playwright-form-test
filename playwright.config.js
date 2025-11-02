a01d5893c215d8b69cb8d7948927dc3605152ae5
Yingjie Yang <102939225+Tiffany678@users.noreply.github.com>
2025-11-02 16:14:47 -0500

    update

--- playwright.config.js
+++ playwright.config.js
@@ -1,17 +1,15 @@
-// export const reporter = [["list"], ["html"]];
-// playwright.config.js
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
-  testDir: './tests',
+  testDir: "./tests",
   use: {
-    baseURL: process.env.BASE_URL || 'https://yang-react.web.app',
-    trace: 'on-first-retry',
+    baseURL: process.env.BASE_URL || "https://yang-react.web.app/",
+    trace: "on-first-retry",
   },
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] }
-    }
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
   ],
 });
